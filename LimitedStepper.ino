44a392ec46d186d31e9a3f637ce81c47a745ac2c
[PC] L. Wagner <lion.wagner@freenet.de>
2018-03-14 20:32:08 +0100

    Adjusted Project to work with Arduino IDE added German commentary

--- LimitedStepper.ino
+++ LimitedStepper.ino
@@ -1,49 +1,67 @@
 #include "Arduino.h"
 #include "LimitedStepperMotor.h"
 
-//
+/*Einstellungen Motor*/
+
+//Speicherort im EEPROM
 const int EEPROMCell = 0;
+//Schritte des Schrittmotors (sollte irgendow drauf bzw. in der Beschreibung stehen)
 const int number_of_Steps = 4800;
-const int Pins_StepperMotor[] = {8,9,10,11};
+//Pins mit den der Motor/Driver/die Steuereinheit verbunden ist (ggf. auf 2 verringern)
+const int Pins_StepperMotor[] = {8, 9, 10, 11};
+//invertiert Hoch und Runter f�r den Motor
 const bool invertiereMotor = false;
+//Grundgeschwindigkeit des Motors in Umdrehungen pro Minute.
+const int speed = 120;
 
-const int Pin_Endschalter = 0;
-const int Pin_ManuelleKonfigurationTrigger = 1;
-const int Pin_HochfahrTrigger=2;
-const int Pin_RunterfahrTrigger=3;
+/*Einstellungen Eing�nge*/
+
+//eingang des Endschalters
+const int Pin_Endschalter = 2;
+//Eingang der das manuelle Kalibrieren starten soll.
+const int Pin_ManuelleKalibrierungTrigger = 3;
+//Eingang der das Hochfahren ausl�sen soll
+const int Pin_HochfahrTrigger = 4;
+//Eingang der das Runterfahren ausl�sen soll
+const int Pin_RunterfahrTrigger = 5;
 
 
-LimitedStepperMotor motor(EEPROMCell,number_of_Steps,Pins_StepperMotor[0],Pins_StepperMotor[1],Pins_StepperMotor[2],Pins_StepperMotor[3]);
+//Motor anlegen, evtl muss hier ebenfalls auf 2 Pins umgestellt werden.
+LimitedStepperMotor motor(EEPROMCell, number_of_Steps, Pins_StepperMotor[0], Pins_StepperMotor[1], Pins_StepperMotor[2], Pins_StepperMotor[3]);
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
+  //Eing�nge auf INPUT schalten
+  pinMode(Pin_Endschalter, INPUT);
+  pinMode(Pin_ManuelleKalibrierungTrigger, INPUT);
+  pinMode(Pin_HochfahrTrigger, INPUT);
+  pinMode(Pin_RunterfahrTrigger, INPUT);
 
-	pinMode(Pin_Endschalter,INPUT);
-	pinMode(Pin_ManuelleKonfigurationTrigger,INPUT);
-	pinMode(Pin_HochfahrTrigger,INPUT);
-	pinMode(Pin_RunterfahrTrigger,INPUT);
+  //motor Konfiguration von oben �bernehmen
+  motor.setDeactivationPin(Pin_Endschalter);
+  motor.setInverted(invertiereMotor);
+  motor.setSpeed(speed);
 
-	motor.setDeactivationPin(Pin_Endschalter);
-	motor.setInverted(invertiereMotor);
-
-	motor.calibrateTop();
+  //startet die Kalibrierung des oberen Anschlags (s. in der Klasse LimitedStepperMotor.cpp)
+  motor.calibrateTop();
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-	if(digitalRead(Pin_ManuelleKonfigurationTrigger) == 1)
-	{
-		delay(1000);
-		motor.calibrateManually(Pin_ManuelleKonfigurationTrigger);
-	}
-	else if(digitalRead(Pin_HochfahrTrigger) == 1)
-	{
-		motor.goUp();
-	}
-	else if(digitalRead(Pin_RunterfahrTrigger) == 1)
-	{
-		motor.goDown();
-	}
+  //Eing�nge verarbeiten
+  if (digitalRead(Pin_ManuelleKalibrierungTrigger) == 1)
+  {
+    delay(1000);
+    motor.calibrateManually(Pin_ManuelleKalibrierungTrigger);
+  }
+  else if (digitalRead(Pin_HochfahrTrigger) == 1)
+  {
+    motor.goUp();
+  }
+  else if (digitalRead(Pin_RunterfahrTrigger) == 1)
+  {
+    motor.goDown();
+  }
 }
--- LimitedStepperMotor.h
+++ LimitedStepperMotor.h
@@ -1,9 +1,9 @@
 /*
- * TorStepper.h
- *
- *  Created on: 14.03.2018
- *      Author: Lion
- */
+   LimitedStepperMotor.h
+
+    Created on: 14.03.2018
+        Author: Lion
+*/
 
 #ifndef LIMITEDSTEPPERMOTOR_H_
 #define LIMITEDSTEPPERMOTOR_H_
@@ -13,61 +13,61 @@
 #include "EEPROM.h"
 
 class LimitedStepperMotor : public Stepper {
-public:
+  public:
+    LimitedStepperMotor(int EEPROMIndex, int number_of_steps, int motor_pin_1, int motor_pin_2);
 
-	LimitedStepperMotor(int EEPROMIndex,int number_of_steps, int motor_pin_1, int motor_pin_2) : Stepper(number_of_steps, motor_pin_1, motor_pin_2){
-		this->EEPROMIndex = EEPROMIndex;
-	}
+    LimitedStepperMotor(int EEPROMIndex, int number_of_steps, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4);
 
-	LimitedStepperMotor(int EEPROMIndex,int number_of_steps, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4)
-		 : Stepper(number_of_steps, motor_pin_1, motor_pin_2,  motor_pin_3,  motor_pin_4){
-		this->EEPROMIndex = EEPROMIndex;
-	}
+    void setDeactivationPin(int pin) {
+      deactivationPin = pin;
+      pinMode(pin, INPUT);
+    };
 
-	void setDeactivationPin(int pin){deactivationPin = pin; pinMode(pin,INPUT); };
+    void calibrateTop();
 
-	void calibrateTop();
+    void setMaxStepNumber(unsigned int steps) {
+      limit = steps;
+    };
 
-	void setMaxStepNumber(int steps) {limit = steps;};
+    void calibrateManually(int pinToWatch);
 
-	void calibrateManually(int pinToWatch);
+    void goDown();
 
-	void goDown();
+    void goUp();
 
-	void goUp();
+    void step(int numberOfSteps);
 
-	void step(int numberOfSteps);
+    void setInverted(bool inverted) {
+      if (inverted) {
+        singleDownStep = -1;
+        singleUpStep = 1;
+      }
+      else {
+        singleDownStep = 1;
+        singleUpStep = -1;
+      }
+    }
 
-	void setInverted(bool inverted) {
-		if(inverted) {
-			singleDownStep = -1;
-			singleUpStep = 1;
-		}
-		else {
-			singleDownStep = 1;
-			singleUpStep = -1;
-		}
-	}
+  private:
+    int deactivationPin = 0; // Pinnummer des Endschalter-Signals
+    unsigned int currentLocation = 0; //enthält die aktuelle Position
+    unsigned int limit = 1;
+    int EEPROMIndex;
 
-private:
-	int inverted = 1;
-	int deactivationPin=0;
-	int currentLocation = 0;
-	int limit = 0;
-	int EEPROMIndex;
-	int singleDownStep = 1;
-	int singleUpStep = -1;
+    //Diese Variablen werden beim invertieren umgedreht
+    int singleDownStep = 1; //Beschreibt die Anzahl an Schritten die einem Schritt nach unten entsprechen soll
+    int singleUpStep = -1;//Beschreibt die Anzahl an Schritten die einem Schritt nach oben entsprechen soll
 
-	void LimitedStepperMotor::saveLimit()
-	{
-		EEPROM.write(EEPROMIndex, limit);
-	}
+    void saveLimit()
+    {
+      EEPROM.write(EEPROMIndex, limit);
+    }
 
-	void LimitedStepperMotor::loadLimit()
-	{
-		int newLimit = EEPROM.read(EEPROMIndex);
-		if(newLimit<=0) newLimit = 0;
-		limit = newLimit;
-	}
+    void loadLimit()
+    {
+      int newLimit = EEPROM.read(EEPROMIndex);
+      if (newLimit <= 0) newLimit = 0; //stellt sicher dass das limit mid. 0 ist
+      limit = newLimit;
+    }
 };
 #endif /* LIMITEDSTEPPERMOTOR_H_ */
